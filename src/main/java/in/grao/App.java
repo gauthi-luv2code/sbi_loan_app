c10328ad05e9e6134cbceb1b50b07494998bfdf8
GRao <gauthamd67@gmail.com>
2024-08-29 15:48:34 +0530

    initial commit

---
+++ src/main/java/in/grao/App.java
@@ -0,0 +1,13 @@
+package in.grao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
